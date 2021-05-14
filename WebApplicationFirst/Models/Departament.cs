5988c2587869657a4e58c841c10a3f5a1374242d
Brun0DevMimura <developer.bruno.mimur@gmail.com>
2021-05-14 09:39:03 -0300

    First Model-Controller-View - Department

--- WebApplicationFirst/Models/Departament.cs
+++ WebApplicationFirst/Models/Departament.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApplicationFirst.Models
+﻿namespace WebApplicationFirst.Models
 {
     public class Departament
     {
