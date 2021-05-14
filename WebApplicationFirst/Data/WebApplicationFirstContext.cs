43a95f4bc1ae39c1d0b241a967b57573c9573b50
Brun0DevMimura <developer.bruno.mimur@gmail.com>
2021-05-14 14:16:09 -0300

    CRUD Scaffolding

---
+++ WebApplicationFirst/Data/WebApplicationFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationFirst.Models;
+
+namespace WebApplicationFirst.Data
+{
+    public class WebApplicationFirstContext : DbContext
+    {
+        public WebApplicationFirstContext (DbContextOptions<WebApplicationFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationFirst.Models.Departament> Departament { get; set; }
+    }
+}
--- WebApplicationFirst/Startup.cs
+++ WebApplicationFirst/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationFirst.Data;
 
 namespace WebApplicationFirst
 {
@@ -33,6 +35,9 @@ namespace WebApplicationFirst
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApplicationFirstContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApplicationFirstContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
