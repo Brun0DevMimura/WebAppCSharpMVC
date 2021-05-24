4959812f682c7bd5f19240681b05f3566782a054
Brun0DevMimura <developer.bruno.mimur@gmail.com>
2021-05-24 09:04:49 -0300

    Other entities and second migration

--- WebApplicationFirst/Data/WebApplicationFirstContext.cs
+++ WebApplicationFirst/Data/WebApplicationFirstContext.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using WebApplicationFirst.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace WebApplicationFirst.Data
 {
     public class WebApplicationFirstContext : DbContext
     {
-        public WebApplicationFirstContext (DbContextOptions<WebApplicationFirstContext> options)
-            : base(options)
+        public WebApplicationFirstContext (DbContextOptions<WebApplicationFirstContext> options) : base(options)
         {
         }
 
         public DbSet<WebApplicationFirst.Models.Departament> Departament { get; set; }
+        public DbSet<WebApplicationFirst.Models.Seller> Seller { get; set; }
+        public DbSet<WebApplicationFirst.Models.SalesRecord> SalesRecords { get; set; }
     }
 }
