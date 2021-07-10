f6e91dd41376a93131b81d166c1a2896c1fd957a
Brun0DevMimura <developer.bruno.mimur@gmail.com>
2021-07-09 22:38:45 -0300

    Simple Create Form

--- WebApplicationFirst/Controllers/SellersController.cs
+++ WebApplicationFirst/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplicationFirst.Models;
 using WebApplicationFirst.Services;
 
 namespace WebApplicationFirst.Controllers
@@ -25,6 +26,14 @@ namespace WebApplicationFirst.Controllers
 
         public IActionResult Create() {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
 
     }
